78fabe559becf264cede7cdf73e9e09ffbc1f8f3
Aleksey Eliseev <alekseyeliseev@ya.ru>
2024-02-12 13:06:15 +0300

    Начальный коммит

---
+++ solutions/13-nested-loops.js
@@ -0,0 +1,5 @@
+const _ = require("lodash"); 
+
+// BEGIN
+
+// END
---
+++ solutions/16-destructuring.js
@@ -0,0 +1,10 @@
+const getDistance = ([x1, y1], [x2, y2]) => {
+    const xs = x2 - x1;
+    const ys = y2 - y1;
+  
+    return Math.sqrt(xs ** 2 + ys ** 2);
+  };
+
+// BEGIN
+
+// END
