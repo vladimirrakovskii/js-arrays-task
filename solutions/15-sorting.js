174509daf8874618ab4c6b3494db64e40056bcb3
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-25 21:21:57 +0300

    Update 15-sorting.js

--- solutions/15-sorting.js
+++ solutions/15-sorting.js
@@ -1,3 +1,19 @@
 // BEGIN
-
+export default function bubbleSort(arr) {
+  const array = [...arr]; // создаем копию массива для сортировки
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        // меняем местами
+        const temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return array;
+}
 // END
