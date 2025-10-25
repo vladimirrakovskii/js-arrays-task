9c73c567d6ceb60666424694d64c12a4a8e3c41f
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-25 18:33:49 +0300

    Update 5-references.js

--- solutions/5-references.js
+++ solutions/5-references.js
@@ -1,3 +1,10 @@
 // BEGIN
-
+export function reverse(arr) {
+  const len = arr.length;
+  for (let i = 0; i < Math.floor(len / 2); i++) {
+    const temp = arr[i];
+    arr[i] = arr[len - 1 - i];
+    arr[len - 1 - i] = temp;
+  }
+}
 // END
