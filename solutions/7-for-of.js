16a04079dcca7b3db6175d1b869e1016793dcb7d
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-25 19:25:50 +0300

    Update 7-for-of.js

--- solutions/7-for-of.js
+++ solutions/7-for-of.js
@@ -1,3 +1,10 @@
-// BEGIN
-
-// END
+export default function calculateAverage(array) {
+  if (array.length === 0) {
+    return null;
+  }
+  let sum = 0;
+  for (const temperature of array) {
+    sum += temperature;
+  }
+  return sum / array.length;
+}
