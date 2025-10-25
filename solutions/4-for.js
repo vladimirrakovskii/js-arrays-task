ec25c65695ac8f72103edea5d914f567d2a68c14
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-25 14:19:06 +0300

    Update 4-for.js

--- solutions/4-for.js
+++ solutions/4-for.js
@@ -1,3 +1,13 @@
 // BEGIN
 
+const addPrefix = (arr, prefix) => {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(`${prefix} ${arr[i]}`);
+  }
+  return result;
+};
+
+export default addPrefix;
+
 // END
