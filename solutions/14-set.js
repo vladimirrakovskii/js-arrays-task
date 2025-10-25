d108e86dae962ba51fba98c044403f57b2badefe
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-25 21:17:18 +0300

    Update 14-set.js

--- solutions/14-set.js
+++ solutions/14-set.js
@@ -1,3 +1,14 @@
 // BEGIN
-
+export default function countUniqChars(str) {
+  const uniqueChars = [];
+  
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (!uniqueChars.includes(char)) {
+      uniqueChars.push(char);
+    }
+  }
+  
+  return uniqueChars.length;
+}
 // END
