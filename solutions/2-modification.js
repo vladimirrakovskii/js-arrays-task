4adb02111963e1c0f1a852b5b3033695df848e61
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-25 13:47:38 +0300

    Update 2-modification.js

--- solutions/2-modification.js
+++ solutions/2-modification.js
@@ -1,3 +1,16 @@
 // BEGIN
+// solutions/2-modification.js
+
+const swap = (arr) => {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const temp = arr[0];
+  arr[0] = arr[arr.length - 1];
+  arr[arr.length - 1] = temp;
+  return arr;
+};
+
+export { swap };
 
 // END
