36dd37f83eaf842fc9f74627c66189031302c6f2
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-25 19:34:20 +0300

    Update 11-build-strings.js

--- solutions/11-build-strings.js
+++ solutions/11-build-strings.js
@@ -1,3 +1,18 @@
 // BEGIN
+export default function buildDefinitionList(definitions) {
+  if (!definitions || definitions.length === 0) {
+    return '';
+  }
 
+  let result = '<dl>';
+
+  for (let i = 0; i < definitions.length; i++) {
+    const [term, description] = definitions[i];
+    result += `<dt>${term}</dt><dd>${description}</dd>`;
+  }
+
+  result += '</dl>';
+
+  return result;
+}
 // END
