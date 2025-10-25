61d94e65916074dca2d1d06d56a882b467b6b792
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-25 21:13:27 +0300

    Update 12-strings.js

--- solutions/12-strings.js
+++ solutions/12-strings.js
@@ -1,3 +1,14 @@
 // BEGIN
+export default function makeCensored(text, stopWords) {
+  // Разбиваем текст на массив слов/подстрок по пробелам
+  const words = text.split(' ');
 
+  // Проходим по каждому слову и проверяем, есть ли оно в списке стоп-слов
+  const censoredWords = words.map(word => 
+    stopWords.includes(word) ? '$#%!' : word
+  );
+
+  // Собираем обратно в строку
+  return censoredWords.join(' ');
+}
 // END
