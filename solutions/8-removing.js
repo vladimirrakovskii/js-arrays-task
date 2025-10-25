503731917171d164d64c2d286730343a5f2600ed
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-25 19:27:29 +0300

    Update 8-removing.js

--- solutions/8-removing.js
+++ solutions/8-removing.js
@@ -1,3 +1,10 @@
 // BEGIN
+export default function getSameParity(numbers) {
+  if (numbers.length === 0) {
+    return [];
+  }
 
+  const firstParity = Math.abs(numbers[0]) % 2;
+  return numbers.filter(item => Math.abs(item) % 2 === firstParity);
+}
 // END
