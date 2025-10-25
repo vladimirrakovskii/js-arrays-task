dd6b0ebee4d85021eb6c9f582d19ba339b08c12e
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-25 14:13:27 +0300

    Update 3-isset.js

--- solutions/3-isset.js
+++ solutions/3-isset.js
@@ -1,3 +1,12 @@
 // BEGIN
 
+const get = (arr, index, defaultValue = null) => {
+  if (index >= 0 && index < arr.length) {
+    return arr[index];
+  }
+  return defaultValue;
+};
+
+export { get };
+
 // END
