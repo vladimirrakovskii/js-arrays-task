55c49e245751e58d7a2373ac2d952b653495fd52
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-25 21:37:37 +0300

    Update 18-spread-operator.js

--- solutions/18-spread-operator.js
+++ solutions/18-spread-operator.js
@@ -1,3 +1,14 @@
 // BEGIN
-
+export function flatten(array) {
+  const result = [];
+  for (const element of array) {
+    if (Array.isArray(element)) {
+      // Раскрываем массив при помощи spread и добавляем элементы в результат
+      result.push(...element);
+    } else {
+      result.push(element);
+    }
+  }
+  return result;
+}
 // END
