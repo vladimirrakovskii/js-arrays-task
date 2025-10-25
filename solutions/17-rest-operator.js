0547b6b5e48411753ae10b1782002137c1b0babc
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-25 21:32:07 +0300

    Update 17-rest-operator.js

--- solutions/17-rest-operator.js
+++ solutions/17-rest-operator.js
@@ -1,3 +1,15 @@
 // BEGIN
-
+export function getMax(array) {
+  if (array.length === 0) {
+    return null;
+  }
+  const [first, ...others] = array; // деструктуризация с rest-оператором
+  let max = first;
+  for (const number of others) {
+    if (number > max) {
+      max = number;
+    }
+  }
+  return max;
+}
 // END
