9b18bde486e94a793215becdb06c9ae2f62c5b96
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-25 18:43:25 +0300

    Update 6-aggregation.js

--- solutions/6-aggregation.js
+++ solutions/6-aggregation.js
@@ -1,3 +1,13 @@
 // BEGIN
-
+export function calculateSum(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return array.reduce((sum, item) => {
+    if (item % 3 === 0) {
+      return sum + item;
+    }
+    return sum;
+  }, 0);
+}
 // END
