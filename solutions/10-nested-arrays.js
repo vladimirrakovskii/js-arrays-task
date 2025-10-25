0fc7ec08ebaa3ae582ce424dc38a75a2b29cd6e6
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-25 19:32:51 +0300

    Update 10-nested-arrays.js

--- solutions/10-nested-arrays.js
+++ solutions/10-nested-arrays.js
@@ -1,3 +1,24 @@
 // BEGIN
+export default function getSuperSeriesWinner(scores) {
+  let canadaWins = 0;
+  let ussrWins = 0;
 
+  for (let i = 0; i < scores.length; i++) {
+    const [canadaScore, ussrScore] = scores[i];
+    if (canadaScore > ussrScore) {
+      canadaWins++;
+    } else if (ussrScore > canadaScore) {
+      ussrWins++;
+    }
+    // ничьи не учитываем, не прибавляем побед
+  }
+
+  if (canadaWins > ussrWins) {
+    return 'canada';
+  } else if (ussrWins > canadaWins) {
+    return 'ussr';
+  } else {
+    return null; // ничья
+  }
+}
 // END
